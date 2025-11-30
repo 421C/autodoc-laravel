75d290762a095a6b991631b3ec37cdeabfd520e2
Edžus <edzus@421c.lv>
2025-12-01 00:48:06 +0200

    improved autodoc:ts-sync command output formatting

--- src/Console/Commands/UpdateTypeScriptStructures.php
+++ src/Console/Commands/UpdateTypeScriptStructures.php
@@ -4,6 +4,7 @@ namespace AutoDoc\Laravel\Console\Commands;
 
 use AutoDoc\Laravel\ConfigLoader;
 use Illuminate\Console\Command;
+use Throwable;
 
 class UpdateTypeScriptStructures extends Command
 {
@@ -18,12 +19,43 @@ class UpdateTypeScriptStructures extends Command
         /** @var ?string */
         $workingDirectory = $this->argument('working_directory');
 
-        $updatedFiles = (new \AutoDoc\Commands\UpdateTypeScriptStructures($config))->run($workingDirectory);
+        $commandOutput = (new \AutoDoc\Commands\UpdateTypeScriptStructures($config))->run($workingDirectory);
 
-        foreach ($updatedFiles as $file) {
-            $this->line('Updated ' . $file['filePath'] . ' (' . $file['processedTags'] . ' tags)');
+        foreach ($commandOutput as $message) {
+            if (isset($message['filePath'])) {
+                $tagsText = $message['processedTags'] . ' tag' . ($message['processedTags'] === 1 ? '' : 's');
+
+                $this->line('Updated <fg=bright-white>' . $this->formatFilePath($message['filePath']) . '</> <fg=gray>(' . $tagsText . ')</>');
+
+            } else if (isset($message['error'])) {
+                if ($message['error'] instanceof Throwable) {
+                    $errorText = $message['error']->getMessage() . ' [' . $message['error']->getFile() . ':' . $message['error']->getLine() . ']';
+
+                } else {
+                    $errorText = $message['error'];
+                }
+
+                /** @phpstan-ignore isset.property */
+                if (isset($this->components)) {
+                    $this->components->error($errorText);
+
+                } else {
+                    $this->error($errorText);
+                }
+            }
         }
 
         return Command::SUCCESS;
+    }
+
+    private function formatFilePath(string $path): string
+    {
+        $basePath = base_path();
+
+        if (str_starts_with($path, $basePath)) {
+            return ltrim(substr($path, strlen($basePath)), '/');
+        }
+
+        return $path;
     }
 }
