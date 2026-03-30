35ad69d9e50b1884818f9a740e8a9a237d933640
Edžus <edzus@421c.lv>
2026-03-30 14:03:06 +0300

    fixed nullable array handling in dot notation parser

--- src/Helpers/DotNotationParser.php
+++ src/Helpers/DotNotationParser.php
@@ -3,7 +3,9 @@
 namespace AutoDoc\Laravel\Helpers;
 
 use AutoDoc\DataTypes\ArrayType;
+use AutoDoc\DataTypes\NullType;
 use AutoDoc\DataTypes\Type;
+use AutoDoc\DataTypes\UnionType;
 
 trait DotNotationParser
 {
@@ -26,11 +28,46 @@ trait DotNotationParser
         }
 
         if ($segments[0] === '*') {
+            $nullable = false;
+
             if (! isset($structure[$segment])) {
                 $structure[$segment] = new ArrayType(itemType: new ArrayType(shape: []));
 
             } else if (! ($structure[$segment] instanceof ArrayType)) {
-                $structure[$segment] = (new ArrayType(itemType: new ArrayType(shape: [])))->setRequired($structure[$segment]->required);
+                $existing = $structure[$segment];
+
+                if ($existing instanceof UnionType) {
+                    $innerArray = null;
+
+                    foreach ($existing->types as $innerType) {
+                        if ($innerType instanceof ArrayType) {
+                            $innerArray = $innerType;
+
+                        } else if ($innerType instanceof NullType) {
+                            $nullable = true;
+                        }
+                    }
+
+                    if ($innerArray !== null) {
+                        if ($innerArray->itemType === null) {
+                            $innerArray->itemType = new ArrayType(shape: []);
+
+                        } else if (! ($innerArray->itemType instanceof ArrayType)) {
+                            $innerArray->itemType = (new ArrayType(shape: []))->setRequired($innerArray->itemType->required);
+
+                        } else if (! $innerArray->itemType->shape) {
+                            $innerArray->itemType = (new ArrayType(shape: []))->setRequired($innerArray->itemType->required);
+                        }
+
+                        $structure[$segment] = $innerArray;
+
+                    } else {
+                        $structure[$segment] = (new ArrayType(itemType: new ArrayType(shape: [])))->setRequired($existing->required);
+                    }
+
+                } else {
+                    $structure[$segment] = (new ArrayType(itemType: new ArrayType(shape: [])))->setRequired($existing->required);
+                }
 
             } else if ($structure[$segment]->itemType === null) {
                 $structure[$segment]->itemType = new ArrayType(shape: []);
@@ -49,10 +86,18 @@ trait DotNotationParser
             if (empty($segments)) {
                 $structure[$segment]->itemType = $type;
 
+                if ($nullable) {
+                    $structure[$segment] = new UnionType([$structure[$segment], new NullType]);
+                }
+
                 return;
             }
 
             $this->dotNotationToNestedArrayType($itemShape, $segments, $type);
+
+            if ($nullable) {
+                $structure[$segment] = new UnionType([$structure[$segment], new NullType]);
+            }
 
         } else {
             if (! isset($structure[$segment])) {
